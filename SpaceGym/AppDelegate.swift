a0b4dfa878de6cd0c7b8b76a85d80822b1bb7a4a
pebruwantoro <pebruwantoro.doni@gmail.com>
2024-05-20 20:55:23 +0700

    first-init

---
+++ SpaceGym/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  SpaceGym
+//
+//  Created by Doni Pebruwantoro on 20/05/24.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
